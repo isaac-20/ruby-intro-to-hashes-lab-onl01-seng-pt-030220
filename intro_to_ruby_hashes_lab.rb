1ed3840d1f305bebdf90b8ec677ac438acce6da4
Isaac Smith <isaacsmith215@aol.com>
2020-03-15 04:04:23 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,17 +1,24 @@
 def new_hash
-
+  hash = {}
+  return hash
 end
 
 def actor
-
+  actor = {name: "Dwayne The Rock Johnson"}
 end
 
 def monopoly
-	monopoly = {}
+	monopoly = {railroads: {}}
 
 end
 
 def monopoly_with_second_tier
+  monopoly = {railroads: {
+    pieces: 4,
+    names:{},
+    rent_in_dollars:{}
+    }
+  }
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
 
 end
