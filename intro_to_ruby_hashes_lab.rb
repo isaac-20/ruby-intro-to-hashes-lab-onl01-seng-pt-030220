a3ed6756742e646ff71aa32fa1c753b9af200d30
Isaac Smith <isaacsmith215@aol.com>
2020-03-15 05:16:10 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,12 +25,7 @@ def monopoly_with_second_tier
       :four_pieces_owned=>200
     },
     :names=>
-    {:reading_railroad=>
-      {
-        "mortgage_value"=>"$100"
-      }
-      
-    }
+    {}
   }
 }
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
