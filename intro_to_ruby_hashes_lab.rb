ad320473fccec09bab721fdd0c69599e65b28346
Isaac Smith <isaacsmith215@aol.com>
2020-03-15 04:18:18 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,12 +18,7 @@ def monopoly_with_second_tier
   {
     :pieces=>4,
     :rent_in_dollars=>{},
-    :names=>
-    {
-      :reading_railroad=>
-      {
-        "mortgage_value"=>"$100"
-      },
+    :names=>{},
       :pennsylvania_railroad=>
       {
         "mortgage_value"=>"$200"
